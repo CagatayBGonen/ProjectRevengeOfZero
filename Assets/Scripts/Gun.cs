da8bd466feb7827049a3f94f1ce19b4ebccb9a2b
Cagatay Bayram Gonen <cagataybayramgonen@gmail.com>
2023-10-06 05:59:50 +0300

    Added First Person Movement

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Gun : MonoBehaviour
+{
+
+    public float damage = 10f;
+    public float range = 100f;
+
+    private InputManager inputManager;
+    public Camera fpsCam;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        inputManager = InputManager.Instance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inputManager.IsPlayerFired())
+        {
+            Shoot();
+        }
+    }
+    void Shoot()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(fpsCam.transform.position,fpsCam.transform.forward, out hit, range))
+        {
+                 
+        }
+    }
+}
